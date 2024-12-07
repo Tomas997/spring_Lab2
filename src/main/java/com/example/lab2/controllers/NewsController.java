28edc27af4fa06d8141de2ac8cc590015fef9c95
Andriy Plaksenkov <plaksenkov.andriy@lll.kpi.ua>
2024-12-07 13:48:40 +0200

    fixed getNewsById (exception didn't appear)

--- src/main/java/com/example/lab2/controllers/NewsController.java
+++ src/main/java/com/example/lab2/controllers/NewsController.java
@@ -1,6 +1,7 @@
 package com.example.lab2.controllers;
 
 import com.example.lab2.models.News;
+import com.example.lab2.services.NewsNotFoundException;
 import com.example.lab2.services.NewsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,14 +17,12 @@ public class NewsController {
 
     private final NewsService newsService;
 
-    // CREATE
     @PostMapping
     public ResponseEntity<String> createNews(@RequestBody News news) {
         newsService.createNews(news);
         return ResponseEntity.status(HttpStatus.CREATED).body("News created successfully.");
     }
 
-    // READ ALL
     @GetMapping
     public ResponseEntity<List<News>> getAllNews() {
         return newsService.getAllNews()
@@ -31,22 +30,19 @@ public class NewsController {
                 .orElse(ResponseEntity.noContent().build());
     }
 
-    // READ BY ID
     @GetMapping("/{id}")
     public ResponseEntity<News> getNewsById(@PathVariable Long id) {
-        return newsService.getNewsById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        News news = newsService.getNewsById(id)
+                .orElseThrow(() -> new NewsNotFoundException(id));
+        return ResponseEntity.ok(news);
     }
 
-    // UPDATE
     @PutMapping("/{id}")
     public ResponseEntity<News> updateNews(@PathVariable Long id, @RequestBody News updatedNews) {
         updatedNews = newsService.updateNews(id, updatedNews);
         return ResponseEntity.ok(updatedNews);
     }
 
-    // DELETE
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteNews(@PathVariable Long id) {
